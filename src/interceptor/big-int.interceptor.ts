843f865023c99588c63b2b58826b1b5e84ca5451
jsw <develjsw@gmail.com>
2024-09-23 22:36:28 +0900

    bigint 처리 interceptor 추가

---
+++ src/interceptor/big-int.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class BigIntInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        return next.handle().pipe(
+            map((data) => {
+                if (data === undefined || data === null) {
+                    return data;
+                }
+
+                // BigInt 포함된 객체를 직렬화
+                return JSON.parse(serializeBigInt(data));
+            })
+        );
+    }
+}
+
+// BigInt 처리 함수
+function serializeBigInt(obj: any): string {
+    return JSON.stringify(obj, (key, value) => (typeof value === 'bigint' ? value.toString() : value));
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { BigIntInterceptor } from './interceptor/big-int.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalInterceptors(new BigIntInterceptor());
 
     const configService = app.get(ConfigService);
 
