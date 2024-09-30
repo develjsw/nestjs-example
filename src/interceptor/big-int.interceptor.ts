ea12d15c163b3468e993d7a8cf859ce076a28afa
jsw <develjsw@gmail.com>
2024-09-30 21:50:16 +0900

    bigint interceptor - datetime 포맷 변경 로직 추가

--- src/interceptor/big-int.interceptor.ts
+++ src/interceptor/big-int.interceptor.ts
@@ -1,24 +1,39 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Injectable()
 export class BigIntInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         return next.handle().pipe(
             map((data) => {
+                // data - undefined, null, 빈 값 예외처리
                 if (data === undefined || data === null) {
                     return data;
                 }
 
-                // BigInt 포함된 객체를 직렬화
-                return JSON.parse(serializeBigInt(data));
+                return JSON.parse(serializeBigIntAndDateTime(data));
             })
         );
     }
 }
 
-// BigInt 처리 함수
-function serializeBigInt(obj: any): string {
-    return JSON.stringify(obj, (key, value) => (typeof value === 'bigint' ? Number(value) : value));
+function serializeBigIntAndDateTime(obj: any): string {
+    return JSON.stringify(obj, (key, value) => {
+        // bigint 처리
+        if (typeof value === 'bigint') {
+            return Number(value);
+        }
+
+        if (typeof value === 'string' && isValidDate(value)) {
+            return moment(value).format('YYYY-MM-DD HH:mm:ss');
+        }
+
+        return value;
+    });
+}
+
+function isValidDate(value: string): boolean {
+    return moment(value, moment.ISO_8601, true).isValid();
 }
